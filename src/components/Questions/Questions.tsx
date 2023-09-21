8b7307377688093d95cce8aefa5d62690fc561c3
Artyomsheq <artimushka@gmail.com>
2023-09-21 23:04:03 +0600

    questions section

---
+++ src/components/Questions/Questions.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import s from "./Questions.module.scss"
+
+import { Collapse } from 'antd';
+const Panel = Collapse.Panel;
+
+const text = `
+  lorem ipsum dolor
+`;
+
+const customPanelStyle = {
+    borderBottom: '1px solid #A3A3A3',
+    background: '#D5D7C4',
+};
+
+const nestedPanelStyle = {
+    borderBottom: 0,
+    background: '#D5D7C4'
+};
+
+const Questions: React.FC = () => {
+
+    return (
+        <div className={s.questions}>
+            <div className={s.questions_description}>
+                <h3>Часто задаваемые вопросы</h3>
+                <p>Быстрые ответы на вопросы, которые могут у вас возникнуть. Не нашли то, что ищите? Закажите бесплатную консультацию.</p>
+            </div>
+            <div className={s.questions_accordion}>
+                <Collapse bordered={false} defaultActiveKey={['1']} expandIcon={({ isActive }) => null}>
+                    <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы аренды</div>} key="1" style={customPanelStyle}>
+                        <Collapse bordered={false} expandIcon={({ isActive }) => null}>
+                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <p>От чего зависит цена аренды?</p></div>} key="nasted" style={customPanelStyle}>
+                                <p>{text}</p>
+                            </Panel>
+                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <p>Когда бронирование считается подтвержденным?</p></div>} key="nasted2" style={nestedPanelStyle}>
+                                <p>{text}</p>
+                            </Panel>
+                        </Collapse>
+                    </Panel>
+                    <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы покупки</div>} key="2" style={customPanelStyle}>
+                        <p>{text}</p>
+                    </Panel>
+                    <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы сервиса</div>} key="3" style={customPanelStyle}>
+                        <p>{text}</p>
+                    </Panel>
+                </Collapse>
+            </div>
+        </div>
+    );
+};
+
+export default Questions;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import dynamic from "next/dynamic";
+import Questions from "@/components/Questions/Questions";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Questions/>
     </>
   );
 }
