ac41e31709741a14744bb59f92928e73ee34c0d0
Artyomsheq <artimushka@gmail.com>
2023-09-22 00:43:26 +0600

    rework

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,12 +1,10 @@
-
 import Statistics from "@/components/Statistics/Statistics";
 import React from "react";
 
 const AboutUs = () => {
   return <div>
-<Statistics/>
-    AboutUs
-    </div>;
+    <Statistics/>
+  </div>;
 };
 
 export default AboutUs;
