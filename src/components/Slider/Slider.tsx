84481c98db7b8894dbe2626887a3bf0e3172f6fd
Katherina <fedoseeva2292@gmail.com>
2023-09-22 18:44:01 +0200

    new mew message

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef, useState } from 'react';
+import s from './Slider.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation, Scrollbar } from 'swiper/modules';
+
+import BuildCard from '@/components/BuildCard/BuildCard';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import Image from 'next/image';
+
+const Slider = () => {
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  useEffect(() => {
+    const updateScreenWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    updateScreenWidth();
+
+    window.addEventListener('resize', updateScreenWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateScreenWidth);
+    };
+  }, []);
+
+  const isWideScreen = screenWidth >= 1024;
+
+  const paginationConfig = isWideScreen
+    ? {
+        type: 'progressbar',
+        clickable: true,
+        
+      }
+    : false;
+
+  return (
+    <>
+      <div className={s.header_slider}>
+        <p>Топ-10 проектов</p>
+        <div className={s.btn_group}>
+          <button className={s.left_btn}>
+            <Image
+              src={'/BtnSliderIcon.png'}
+              width={16}
+              height={16}
+              alt="slider icon"
+            />
+          </button>
+          <button className={s.right_btn}>
+            <Image
+              src={'/BtnSliderIcon.png'}
+              width={16}
+              height={16}
+              alt="slider icon"
+              className={s.right_icon_img}
+            />
+          </button>
+        </div>
+      </div>
+      <div className="slider-progress-wrap">
+        <div className="slider-progress"></div>
+        <div className="slider-progress2"></div>
+        <div className="slider-scrollbar"></div>
+      </div>
+      <Swiper
+        slidesPerView={1}
+        breakpoints={{
+          428: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        scrollbar={{
+          draggable: true
+          
+        }}
+        modules={[Scrollbar]}
+      >
+        <SwiperSlide>
+          <BuildCard />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BuildCard />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BuildCard />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BuildCard />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BuildCard />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BuildCard />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Slider;
