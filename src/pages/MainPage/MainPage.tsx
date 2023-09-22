881b5e4f6b3a8e3cf5404149444d1860718c1e17
KseniaShepherd <colorleafs8@gmail.com>
2023-09-22 15:39:44 +0200

    added import in MainPage

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import Services from '@/components/Services/Services';
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <Services/>
+        </div>
+    );
+};
+
+export default MainPage;
