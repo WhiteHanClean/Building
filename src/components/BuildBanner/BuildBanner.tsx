36030359077254e33e30b70e699976e3c9e8f80a
WhiteHanClean <whitehanclean@gmail.com>
2023-09-23 18:26:11 +0600

    add new features

---
+++ src/components/BuildBanner/BuildBanner.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import s from "./BuildBanner.module.scss";
+import Image from "next/image";
+import CustomTabs from "@/shared/ui/Tabs/Tabs";
+import CustomSelect from "@/shared/ui/Select/Select";
+
+const BuildBanner = () => {
+  const tabData = [
+    { key: 1, title: "Купить" },
+    { key: 2, title: "Арендовать" },
+  ];
+
+  const contentData = {
+    "1": [<CustomSelect />, <CustomSelect />],
+    "2": [<CustomSelect />, <CustomSelect />],
+  };
+
+  return (
+    <section className={s.build_content}>
+      <div className={s.build_content_text}>
+        <div className={s.build_content_title}>
+          <h1>Откройте дверь в мир райской недвижимости вместе с нами!</h1>
+        </div>
+        <h1>Откройте дверь в мир райской недвижимости вместе с нами!</h1>
+      </div>
+
+      <div className={s.build_description}>
+        <p>
+          {" "}
+          Property Library Phuket — ваш ключ к успешным инвестициям и комфортной
+          жизни на острове Пхукет.
+        </p>
+        <button className={s.build_arrow}>
+          <Image
+            src={"/arrowTop.png"}
+            width={16}
+            height={16}
+            alt="logo"
+          ></Image>
+        </button>
+      </div>
+
+      <div className={s.build_application}>
+        <h2>Фильтр для поиска вашего идеального жилья</h2>
+
+        <CustomTabs tabData={tabData} contentData={contentData} />
+      </div>
+    </section>
+  );
+};
+
+export default BuildBanner;
--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,10 +1,14 @@
 import Statistics from "@/components/Statistics/Statistics";
+import WelcomeBanner from "@/components/WelcomeBanner/WelcomeBanner";
 import React from "react";
 
 const AboutUs = () => {
-  return <div>
-    <Statistics/>
-  </div>;
+  return (
+    <div>
+      <WelcomeBanner />
+      <Statistics />
+    </div>
+  );
 };
 
 export default AboutUs;
