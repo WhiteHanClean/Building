cecbb7df825788cbf4283c04f6a337533322d571
Katherina <fedoseeva2292@gmail.com>
2023-09-24 09:58:04 +0200

    some changes

--- src/components/Stages/StageCard/StageCard.tsx
+++ src/components/Stages/StageCard/StageCard.tsx
@@ -9,18 +9,17 @@ interface StageCardProps {
 
 const StageCard = ({ number, headerText, description }: StageCardProps) => {
   return (
-      <div className={s.stage_card_container}>
-        {/* header */}
-        <div className={s.stage_header_info}>
-          <div className={s.stage_number_wrapper}>
-            <h1 className={s.stage_number}>{number}</h1>
-          </div>
-          <h1 className={s.stage_text}>{headerText}</h1>
+    <div className={s.stage_card_container}>
+      {/* header */}
+      <div className={s.stage_header_info}>
+        <div className={s.stage_number_wrapper}>
+          <h1 className={s.stage_number}>{number}</h1>
         </div>
-        {/* description */}
-        <p className={s.stage_description}>{description}</p>
+        <h1 className={s.stage_text}>{headerText}</h1>
       </div>
-
+      {/* description */}
+      <p>{description}</p>
+    </div>
   );
 };
 
