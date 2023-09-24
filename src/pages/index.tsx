87172d5922fff0634a3699335e18840c9c76ecc1
Katherina <fedoseeva2292@gmail.com>
2023-09-24 11:00:09 +0200

    fixed conflict

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import dynamic from "next/dynamic";
-<<<<<<< HEAD
+
 import Slider from '@/components/Slider/Slider'
-=======
+
 import TypesOfServices from "@/components/TypesOfServices/TypesOfServices";
 import Service from "@/components/Services/Service";
 import BuildBanner from "@/components/BuildBanner/BuildBanner";
 import Statistics from "@/components/Statistics/Statistics";
 import Consultation from "@/components/Consultation/Consultation";
->>>>>>> 384e8d16d20913ba8776c24effe4660d8613f24e
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,16 +26,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<<<<<<< HEAD
+
       <Slider/>
-=======
+
       <BuildBanner />
 
       <TypesOfServices />
       <Statistics />
       <Service />
       <Consultation />
->>>>>>> 384e8d16d20913ba8776c24effe4660d8613f24e
     </>
   );
 }
