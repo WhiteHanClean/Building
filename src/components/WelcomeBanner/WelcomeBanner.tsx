be9e3fe35837d71e7215b8e17c015634f2b2e2ac
KseniaShepherd <colorleafs8@gmail.com>
2023-09-24 20:23:30 +0200

    fixed img size

--- src/components/WelcomeBanner/WelcomeBanner.tsx
+++ src/components/WelcomeBanner/WelcomeBanner.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import style from "./WelcomeBanner.module.scss";
+import s from "./WelcomeBanner.module.scss";
 
 const WelcomeBanner = () => {
+  const tabData = [
+    { key: "tab1", title: "Таб 1" },
+    { key: "tab2", title: "Таб 2" },
+    { key: "tab3", title: "Таб 3" },
+  ];
+
+  const contentData = [
+    <div>Содержимое Таба 1</div>,
+    <div>Содержимое Таба 2</div>,
+    <div>Содержимое Таба 3</div>,
+  ];
   return (
-    <div className={style.bgImg}>
-      <div className={style.textContent}>
-        <div className={style.welcomSpeech}>
-          <h1>Добро пожаловать в Property Library Phuket</h1>
-          <p>
-            Ваш ключ к успешным инвестициям и комфортной жизни на острове
-            Пхукет.
-          </p>
-        </div>
-        <div className={style.founder}>
-          <h2>Эльнур Ханкишиев</h2>
-          <p>Основатель компании Property Library Phuket</p>
-        </div>
+    <div className={s.welcome_banner}>
+      <div className={s.welcome_text_content}>
+        <h1>Добро пожаловать в Property Library Phuket</h1>
+        <h4>
+          Ваш ключ к успешным инвестициям <br/> и комфортной жизни на острове Пхукет.
+        </h4>
+      </div>
+
+      <div className={s.founder}>
+        <h2>Эльнур Ханкишиев</h2>
+        <h5>Основатель компании Property Library Phuket</h5>
       </div>
     </div>
   );
