0d28b601acbdaedf8dfd1f35dbde20723a799885
Artyomsheq <artimushka@gmail.com>
2023-09-25 21:31:45 +0600

    first commit

---
+++ src/components/AsideButton/AsideButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "./AsideButton.module.scss"
+
+const AsideButton: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AsideButton;
