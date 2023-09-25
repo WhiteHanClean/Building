52ef4a899a4f67651082de69e94f3bf760bab51f
KseniaShepherd <colorleafs8@gmail.com>
2023-09-25 10:48:06 +0200

    fixed the adaptability

--- src/components/FounderQuoteBanner/FounderQuoteBanner.tsx
+++ src/components/FounderQuoteBanner/FounderQuoteBanner.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import style from './FounderQuoteBanner.module.scss'
+import React from "react";
+import style from "./FounderQuoteBanner.module.scss";
 
 const FounderQuoteBanner = () => {
-    return (
-        <div>
-             <div className={style.bgImg}>
-      <div className={style.textContent}>
-        <div className={style.founderQuote}>
-          <p>
-          “Сделайте правильный выбор с Property Library Phuket — вашим надежным партнером в мире недвижимости.”
-          </p>
-        </div>
-        <div className={style.founder}>
-          <h2>Эльнур Ханкишиев</h2>
-          <p>Основатель компании Property Library Phuket</p>
-        </div>
+  return (
+    <div className={style.founderQuoteBanner}>
+      <div className={style.founderQuoteBanner_text_content}>
+        <h4>
+          “Сделайте правильный выбор с Property Library Phuket — вашим надежным
+          партнером в мире недвижимости.”
+        </h4>{" "}
+      </div>
+
+      <div className={style.founderQuote_founder}>
+        <h2>Эльнур Ханкишиев</h2>
+        <h5>Основатель компании Property Library Phuket</h5>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default FounderQuoteBanner;
