f5f0d1cd39fea7c96713474560131293f4bd6e44
Artyomsheq <artimushka@gmail.com>
2023-09-26 04:00:53 +0600

    rework question seqction

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./Questions.module.scss"
 
 import { Collapse } from 'antd';
 const Panel = Collapse.Panel;
 
-const text = `
-  lorem ipsum dolor
-`;
+const text = (
+    <ul >
+        <li>На стоимость аренды влияют следующие факторы:</li>
+        <li>сезонность: в высокий и летний сезон цена может отличаться более чем в 3 раза. Периодичность изменения цен устанавливают собственники недвижимости, ориентируясь на спрос и туристический поток;</li>
+        <li>срок аренды: чем продолжительнее отдых, тем ниже стоимость аренды за сутки. На недвижимость, расположенную на территориях отелей, это правило не распространяется;</li>
+        <li>даты заезда: вероятность дополнительных скидок на даты, удобные для владельца объекта, очень высока;</li>
+        <li>акции: максимальное количество выгодных предложений приходится на летний и средний сезоны.</li>
+    </ul>
+);
 
 const customPanelStyle = {
     borderBottom: '1px solid #A3A3A3',
@@ -20,6 +26,12 @@ const nestedPanelStyle = {
 
 const Questions: React.FC = () => {
 
+    const [active, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!active);
+    };
+
     return (
         <div className={s.questions}>
             <div className={s.questions_description}>
@@ -30,19 +42,19 @@ const Questions: React.FC = () => {
                 <Collapse bordered={false} expandIcon={({ isActive }) => null}>
                     <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы аренды</div>} key="1" style={customPanelStyle}>
                         <Collapse bordered={false} expandIcon={({ isActive }) => null}>
-                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <p>От чего зависит цена аренды?</p></div>} key="nasted" style={customPanelStyle}>
-                                <p>{text}</p>
+                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <h3>От чего зависит цена аренды?</h3></div>} key="nasted" style={customPanelStyle}>
+                                <p className="questions_accordion_button_title">{text}</p>
                             </Panel>
-                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <p>Когда бронирование считается подтвержденным?</p></div>} key="nasted2" style={nestedPanelStyle}>
-                                <p>{text}</p>
+                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <h3>Когда бронирование считается подтвержденным?</h3></div>} key="nasted2" style={nestedPanelStyle}>
+                                <p className="questions_accordion_button_title">lorem ipsum dolor</p>
                             </Panel>
                         </Collapse>
                     </Panel>
                     <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы покупки</div>} key="2" style={customPanelStyle}>
-                        <p>{text}</p>
+                        <p className="questions_accordion_button_title">lorem ipsum dolor</p>
                     </Panel>
                     <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы сервиса</div>} key="3" style={customPanelStyle}>
-                        <p>{text}</p>
+                        <p className="questions_accordion_button_title">lorem ipsum dolor</p>
                     </Panel>
                 </Collapse>
             </div>
