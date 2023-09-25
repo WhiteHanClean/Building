f506f146ef4a8e2535798abba05da60e943497fc
Artyomsheq <artimushka@gmail.com>
2023-09-26 01:56:46 +0600

    asideButton ui

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import s from "./Layout.module.scss";
+import AsideButton from "../shared/ui/AsideButton/AsideButton";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
+      <AsideButton/>
        <div className={s.content}>{children}</div>
       <Footer />
     </>
