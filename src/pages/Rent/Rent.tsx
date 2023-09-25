3ab2afb1585f81fc09ac646a3033b9aa940faf28
Max Arbakov <fefaodessa@gmail.com>
2023-09-25 23:08:18 +0300

    add list offers

--- src/pages/Rent/Rent.tsx
+++ src/pages/Rent/Rent.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import ListOffers from "@/components/ListOffers/ListOffers";
+import React from "react";
 
 const Rent = () => {
-    return (
-        <div>
-            Rent
-        </div>
-    );
+  return (
+    <div>
+      Rent
+      <ListOffers />
+    </div>
+  );
 };
 
 export default Rent;
