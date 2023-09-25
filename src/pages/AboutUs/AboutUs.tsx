b652e97ca70c2698ce25021e5315bf0313fe082f
L1z1ng3r-sswe <krasavachic@gmail.com>
2023-09-25 19:13:21 +0600

    listing_profitable-cooperation

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,10 +1,14 @@
+import Benefits from "@/components/Benefits/Benefits";
 import Statistics from "@/components/Statistics/Statistics";
 import React from "react";
 
 const AboutUs = () => {
-  return <div>
-    <Statistics/>
-  </div>;
+  return (
+    <div>
+      <Statistics />
+      <Benefits />
+    </div>
+  );
 };
 
 export default AboutUs;
