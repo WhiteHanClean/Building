3fb626e3b9d7ff158753c08580341a3b58793759
KseniaShepherd <colorleafs8@gmail.com>
2023-09-25 23:14:42 +0200

    added conditional rendering of SocialNetworkBlock to about us pages

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,16 +1,27 @@
 import FullService from "@/components/FullService/FullService";
 import Statistics from "@/components/Statistics/Statistics";
 import WelcomeBanner from "@/components/WelcomeBanner/WelcomeBanner";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SocialNetworkBlock from "../../components/SocialNetworkBlock/SocialNetworkBlock";
 
 const AboutUs = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = (event: UIEvent) => {
+      setWidth((event.target as Window).innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div>
       <WelcomeBanner />
       <Statistics />
       <FullService />
-      <SocialNetworkBlock />
+      {width <= 768 ? "" : <SocialNetworkBlock />}
     </div>
   );
 };
