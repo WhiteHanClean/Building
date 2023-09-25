e2a4bba8a7fe819501c00ac3e479771820c7d756
KseniaShepherd <colorleafs8@gmail.com>
2023-09-25 12:26:00 +0200

    fixed container bottom margin

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import s from "./Layout.module.scss";
+import MainPage from "@/pages/MainPage/MainPage";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-        <div className={s.content}>{children}</div>
+        <div className={s.content}>{children}
+        <MainPage/></div>
      
       <Footer />
     </>
