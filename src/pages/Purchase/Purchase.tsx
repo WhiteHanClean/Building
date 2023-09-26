b0cd8f24d9d848851a857b82af9fbf96504fd1fd
Artyomsheq <artimushka@gmail.com>
2023-09-26 21:35:15 +0600

    rework

--- src/pages/Purchase/Purchase.tsx
+++ src/pages/Purchase/Purchase.tsx
@@ -1,11 +1,9 @@
-import PropertyDesc from '@/components/PropertyDesc/PropertyDesc';
 import React from 'react';
 
 const Purchase = () => {
     return (
         <div>
             Purchase
-            <PropertyDesc/>
         </div>
     );
 };
