a58c9835579d5840b32b39ba72f9d14a31e0ae5a
KseniaShepherd <colorleafs8@gmail.com>
2023-09-27 10:39:15 +0200

    added searchBar to BuildBanner

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select } from "antd";
 import s from './Select.module.scss'
+import Image from "next/image";
 
 const { Option } = Select;
 
-const CustomSelect: React.FC = () => {
+interface CustomSelectProps {
+  options: string[];
+  defaultValue?: string;
+  onChange?: (value: string) => void;
+  onBlur?:React.FocusEventHandler<HTMLElement>;
+  value:string;
+}
 
+const CustomSelect: React.FC<CustomSelectProps> = ({options, defaultValue, onChange, onBlur, value}) => {
+  const [isOpen, setIsOpen] = useState(false);
   // Обработчик изменения значения в Select
   const handleChange = (value: string) => {
     console.log(`Selected: ${value}`);
+    if (onChange) {
+      onChange(value);
+    }
   };
+  const handleDropdownVisibleChange = (open: boolean) => {
+    setIsOpen(open);
+  };
+
+  const arrowStyle = isOpen ? { transform: 'rotate(180deg)' } : {};
+
 
   return (
     <div className={s.custom_select_container}>
       <Select
-        defaultValue="Option 1"
-        style={{ width: 200 }}
+       className={s.customSelect}
+       value={value || defaultValue} 
         onChange={handleChange}
+        onBlur={onBlur}
+        onDropdownVisibleChange={handleDropdownVisibleChange}
+        suffixIcon={ <Image
+          src={"/evaArrowUpFill1.png"}
+          width={8}
+          height={6}
+          alt="arrow"
+          style={arrowStyle}
+        ></Image>} 
       >
-        <Option value="Option 1">Option 1</Option>
-        <Option value="Option 2">Option 2</Option>
-        <Option value="Option 3">Option 3</Option>
+        {options.map((option, index) => (
+          <Option key={index} value={option}>
+            {option}
+          </Option>
+        ))}
       </Select>
     </div>
   );
