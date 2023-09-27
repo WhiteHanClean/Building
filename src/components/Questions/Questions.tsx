2f805998c81b7a96bdd9a8eeaf5d14e3a9016d65
KseniaShepherd <colorleafs8@gmail.com>
2023-09-27 12:49:07 +0200

    fixed icons

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from "./Questions.module.scss"
 
 import { Collapse } from 'antd';
@@ -13,12 +13,28 @@ const customPanelStyle = {
     background: '#D5D7C4',
 };
 
-const nestedPanelStyle = {
-    borderBottom: 0,
-    background: '#D5D7C4'
-};
-
 const Questions: React.FC = () => {
+    const [activePanels, setActivePanels] = useState<string | string[]>([]);
+
+    const togglePanel = (key: string | string[]) => {
+      if (typeof key === "string") {
+        if (activePanels === key) {
+          setActivePanels([]);
+        } else {
+          setActivePanels(key);
+        }
+      } else {
+        setActivePanels(key);
+      }
+    };
+  
+    const isPanelActive = (key: string) => {
+      if (typeof activePanels === "string") {
+        return activePanels === key;
+      } else {
+        return activePanels.includes(key);
+      }
+    };
 
     return (
         <div className={s.questions}>
@@ -27,21 +43,47 @@ const Questions: React.FC = () => {
                 <p>Быстрые ответы на вопросы, которые могут у вас возникнуть. Не нашли то, что ищите? Закажите бесплатную консультацию.</p>
             </div>
             <div className={s.questions_accordion}>
-                <Collapse bordered={false} expandIcon={({ isActive }) => null}>
-                    <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы аренды</div>} key="1" style={customPanelStyle}>
+                <Collapse bordered={false} expandIcon={({ isActive }) => null}   activeKey={activePanels}
+          onChange={togglePanel}>
+                    <Panel header={
+                        <div className={s.questions_accordion_button}>
+                            <img src={isPanelActive('1') ? "/accordionMinusBtn.svg" : "/accordionBtn.svg"} alt="accordionBtn" />
+                            Вопросы аренды
+                        </div>
+                    } key="1" style={customPanelStyle}>
                         <Collapse bordered={false} expandIcon={({ isActive }) => null}>
-                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <p>От чего зависит цена аренды?</p></div>} key="nasted" style={customPanelStyle}>
+                            <Panel header={
+                                <div className={s.questions_accordion_button}>
+                                    <img src="/accordionBtn.svg" alt="accordionBtn" />
+                                    <p>От чего зависит цена аренды?</p>
+                                </div>
+                            } key="nasted" style={customPanelStyle}>
                                 <p>{text}</p>
                             </Panel>
-                            <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" /> <p>Когда бронирование считается подтвержденным?</p></div>} key="nasted2" style={nestedPanelStyle}>
+                            <Panel header={
+                                <div className={s.questions_accordion_button}>
+                                    <img src="/accordionBtn.svg" alt="accordionBtn" />
+                                    <p>Когда бронирование считается подтвержденным?</p>
+                                </div>
+                            } key="nasted2" style={customPanelStyle}>
                                 <p>{text}</p>
                             </Panel>
                         </Collapse>
                     </Panel>
-                    <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы покупки</div>} key="2" style={customPanelStyle}>
+                    <Panel header={
+                        <div className={s.questions_accordion_button}>
+                            <img src={isPanelActive('2') ? "/accordionMinusBtn.svg" : "/accordionBtn.svg"} alt="accordionBtn" />
+                            Вопросы покупки
+                        </div>
+                    } key="2" style={customPanelStyle}>
                         <p>{text}</p>
                     </Panel>
-                    <Panel header={<div className={s.questions_accordion_button}><img src="/accordionBtn.svg" alt="accordionBtn" />Вопросы сервиса</div>} key="3" style={customPanelStyle}>
+                    <Panel header={
+                        <div className={s.questions_accordion_button}>
+                            <img src={isPanelActive('3') ? "/accordionMinusBtn.svg" : "/accordionBtn.svg"} alt="accordionBtn" />
+                            Вопросы сервиса
+                        </div>
+                    } key="3" style={customPanelStyle}>
                         <p>{text}</p>
                     </Panel>
                 </Collapse>
