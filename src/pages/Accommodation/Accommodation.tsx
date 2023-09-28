3368c05a5cfa251a763d5dc859ced198c4226dcb
WhiteHanClean <whitehanclean@gmail.com>
2023-09-28 19:36:36 +0600

    ok

--- src/pages/Accommodation/Accommodation.tsx
+++ src/pages/Accommodation/Accommodation.tsx
@@ -1,10 +1,16 @@
+import Benefits from "@/components/Benefits/Benefits";
+import Consultation from "@/components/Consultation/Consultation";
 import ListingHero from "@/components/ListingHero/ListingHero";
+import Questions from "@/components/Questions/Questions";
 import React from "react";
 
 const Accommodation = () => {
   return (
     <div>
       <ListingHero />
+      <Benefits />
+      <Questions/>
+      <Consultation/>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,6 @@ export default function Home() {
       <Slider />
       <TypesOfServices />
       <Stages/>
-      <Benefits />
       <Service />
       <SocialNetworkBlock/>
       <Questions/>
