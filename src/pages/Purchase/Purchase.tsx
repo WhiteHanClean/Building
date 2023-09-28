fa2ab2bc71ae6ee1c7f83954348be6f9a4269f4b
Max Arbakov <fefaodessa@gmail.com>
2023-09-28 19:52:19 +0300

    fix
    delete in Purchase page

--- src/pages/Purchase/Purchase.tsx
+++ src/pages/Purchase/Purchase.tsx
@@ -1,12 +1,10 @@
 import FilterForm from "@/components/FilterForm/FilterForm";
 import ListOffers from "@/components/ListOffers/ListOffers";
-import ProductSlider from "@/components/ProductSlider/ProductSlider";
 import React from "react";
 
 const Purchase = () => {
   return (
     <div>
-      <ProductSlider />
       <FilterForm titleSection="Покупка недвижимости" />
       <ListOffers />
     </div>
