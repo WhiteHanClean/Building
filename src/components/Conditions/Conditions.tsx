999855481f5301860a498eb2d78a13109f0a44d1
Artyomsheq <artimushka@gmail.com>
2023-09-29 18:32:31 +0600

    Conditions section

---
+++ src/components/Conditions/Conditions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "@/components/Conditions/Conditions"
+
+const Conditions: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Conditions;
