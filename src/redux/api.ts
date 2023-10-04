350794650c53a2373b144ce1e3f0a5fa5b64eea6
L1z1ng3r-sswe <krasavachic@gmail.com>
2023-10-04 14:41:54 +0600

    pagination done

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,96 +1,98 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface RealEstate {
-    _id: string;
-    title: string;
-    price: number;
-    roomsAmount: number;
-    landArea: number;
-    builtUpArea: number;
-    kitchenArea: string;
-    floor: string;
-    bathroomAmount: string;
-    balconyAmount: string;
-    buildingType: string;
-    yearBuilt: string;
-    description: string;
-    location: string;
-    hasRestaurant: boolean; // Fixed typo
-    hasParking: boolean;
-    hasSpa: boolean;
-    hasCommunalPool: boolean;
-    hasGym: boolean;
-    hasClub: boolean;
-    __v: number;
-    mainImage: string;
-    alt: string;
+  _id: string;
+  title: string;
+  price: number;
+  roomsAmount: number;
+  landArea: number;
+  builtUpArea: number;
+  kitchenArea: string;
+  floor: string;
+  bathroomAmount: string;
+  balconyAmount: string;
+  buildingType: string;
+  yearBuilt: string;
+  description: string;
+  location: string;
+  hasRestaurant: boolean; // Fixed typo
+  hasParking: boolean;
+  hasSpa: boolean;
+  hasCommunalPool: boolean;
+  hasGym: boolean;
+  hasClub: boolean;
+  __v: number;
+  mainImage: string;
+  alt: string;
 }
 
 export interface FilterParams {
-    currentPage?: number;
-    buildingType?: string;
-    location?: string;
-    roomsAmount?: number;
-    // Additional characteristics
-    builtUpArea?: number;
-    landArea?: number;
-    price?: number;
-    isFilter: boolean;
+  currentPage?: number;
+  limit?: number;
+  buildingType?: string;
+  location?: string;
+  roomsAmount?: number;
+  // Additional characteristics
+  builtUpArea?: number;
+  landArea?: number;
+  price?: number;
+  isFilter: boolean;
 }
 
 interface IGet {
-    filterParams?: FilterParams;
-    currentPage: number;
-    isRent?: boolean;
+  filterParams?: FilterParams;
+  currentPage: number;
+  isRent?: boolean;
+  limit: number;
 }
 
 export const api = createApi({
-    reducerPath: "libraryPhuket",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://propertylibphuket-production.up.railway.app/",
+  reducerPath: "libraryPhuket",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://propertylibphuket-production.up.railway.app/",
+  }),
+  tagTypes: ["Reals"],
+  endpoints: (builder) => ({
+    getAllOffers: builder.query<RealEstate[], void>({
+      query: () => "realEstates/",
+      providesTags: ["Reals"],
     }),
-    tagTypes: ["Reals"],
-    endpoints: (builder) => ({
-        getAllOffers: builder.query<RealEstate[], void>({
-            query: () => "realEstates/",
-            providesTags: ["Reals"],
-        }),
 
-        getFilteredOffers: builder.query<RealEstate[], IGet>({
-            query: ({ currentPage, isRent, filterParams }) => ({
-                url: `realEstates/`,
-                params: {
-                    ...filterParams,
-                    _page: currentPage,
-                    _limit: 9,
-                    isRent: isRent ?? false, // Default to false if isRent is not provided
-                },
-                providesTags: ["Reals"],
-            }),
-        }),
-
-        getUnFilteredOffers: builder.query<RealEstate[], IGet>({
-            query: ({ currentPage, isRent }) => ({
-                url: `realEstates/`,
-                params: {
-                    _page: currentPage,
-                    _limit: 9,
-                    isRent: isRent ?? false, // Default to false if isRent is not provided
-                },
-                providesTags: ["Reals"],
-            }),
-        }),
-
-        // getOneOffers: builder.query<RealEstate[], { id: string }>({
-        //   query: ({ id }) => `realEstates/_id=${id}`,
-        //   providesTags: ["Reals"],
-        // }),
+    getFilteredOffers: builder.query<RealEstate[], IGet>({
+      query: ({ currentPage, isRent, filterParams, limit }) => ({
+        url: `realEstates/`,
+        params: {
+          ...filterParams,
+          _page: currentPage,
+          _limit: limit,
+          isRent: isRent ?? false, // Default to false if isRent is not provided
+        },
+        providesTags: ["Reals"],
+      }),
     }),
+
+    getUnFilteredOffers: builder.query<RealEstate[], IGet>({
+      query: ({ currentPage, isRent, limit }) => ({
+        url: `realEstates/`,
+        params: {
+          _page: currentPage,
+          _limit: limit,
+          isRent: isRent ?? false, // Default to false if isRent is not provided
+        },
+        providesTags: ["Reals"],
+      }),
+    }),
+
+    // getOneOffers: builder.query<RealEstate[], { id: string }>({
+    //   query: ({ id }) => `realEstates/_id=${id}`,
+    //   providesTags: ["Reals"],
+    // }),
+  }),
 });
 
 export const {
-    useGetAllOffersQuery,
-    useGetFilteredOffersQuery,
-    // useGetOneOffersQuery,
-    useGetUnFilteredOffersQuery,
+  useGetAllOffersQuery,
+  useGetFilteredOffersQuery,
+  // useGetOneOffersQuery,
+  useGetUnFilteredOffersQuery,
 } = api;
