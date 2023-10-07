067aec8c57191ad3255e0c22a423318d323261e5
WhiteHanClean <whitehanclean@gmail.com>
2023-10-08 01:57:07 +0600

    fix modal

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,14 +3,13 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import s from "./Layout.module.scss";
 import AsideButton from "../shared/ui/AsideButton/AsideButton";
-import MainPage from "@/pages/MainPage/MainPage";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-      <AsideButton/>
-       <div className={s.content}>{children}</div>
+      <div className={s.content}>{children}</div>
+      <AsideButton />
       <Footer />
     </>
   );
