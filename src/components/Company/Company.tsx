727824331b762d44c1587e126af13c761879e77e
L1z1ng3r-sswe <krasavachic@gmail.com>
2023-10-20 23:31:54 +0600

    abput us / about company block done

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -4,29 +4,59 @@ import { useTranslation } from "react-i18next";
 
 const Company: React.FC = () => {
   const { t } = useTranslation();
+
   return (
-    <div className={s.company}>
-      <div className={s.company_items}>
-        <h3>{t("aboutUs.company.title")}</h3>
-        <p>
-          {t("aboutUs.company.descriptionPart1")}{" "}
-          {t("aboutUs.company.descriptionPart2")}
-        </p>
-        <div className={s.company_items_imgs}>
-          <div className={s.company_items_img}>
-            <img src="/peopleLaptop.png" alt="peopleLaptop" />
-            <p>{t("aboutUs.company.descriptionPart1")}</p>
-          </div>
-          <div className={s.company_items_img}>
-            <img src="/familyMan.png" alt="familyMan" />
-            <p>{t("aboutUs.company.descriptionPart2")}</p>
-          </div>
+    <section className={s.section}>
+      <div className={s.abourUs}>
+        <div>
+          <h3>{t("aboutUs.company.title")}</h3>
+          <p>{t("aboutUs.company.descriptionPart1")}</p>
+        </div>
+        <div className={s.abourUs_img}>
+          <div></div>
         </div>
       </div>
-      <div className={s.company_img}>
-        <img src="/man.png" alt="man" />
+
+      {/* first section ============================ */}
+
+      <div className={s.firstSection}>
+        <div></div>
       </div>
-    </div>
+
+      {/* second section ============================ */}
+
+      <div className={s.secondSection}>
+        {/*  */}
+
+        <div className={s.secondSection_title}>
+          <div>
+            <h3>{t("aboutUs.company.title")}</h3>
+            <p>{t("aboutUs.company.descriptionPart1")}</p>
+          </div>
+          <div className={s.secondSection_img}>
+            <div></div>
+          </div>
+        </div>
+
+        <div className={s.secondSection_history}>
+          <h3></h3>
+          <p></p>
+          <p></p>
+        </div>
+
+        <div className={s.secondSection_target}>
+          <h3></h3>
+          <p></p>
+          <p></p>
+        </div>
+
+        <div className={s.secondSection_familyImg}></div>
+
+        {/*  */}
+      </div>
+
+      {/*  */}
+    </section>
   );
 };
 
